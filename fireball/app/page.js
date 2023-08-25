0ad1777fcb43da82fbadf274e916a03fb792d5e5
AmiRobinsonMuto <amimuto1002@gmail.com>
2023-08-25 14:17:50 -0700

    Add Display.jsx component to render

--- fireball/app/page.js
+++ fireball/app/page.js
@@ -1,8 +1,9 @@
+import Display from "./_components/summary_metrics/Display";
 
 export default function Home() {
   return (
-    <main>
-      Fireballs!
+    <main className="grid grid-cols-4 gap-4">
+      <Display />
     </main>
-  )
+  );
 }
