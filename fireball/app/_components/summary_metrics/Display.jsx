53602663ff5d4b39c4bffa4d03879ba19a8fd7a6
AmiRobinsonMuto <amimuto1002@gmail.com>
2023-08-25 14:17:28 -0700

    Add basic logics and UI to toggle map and bar graph

--- fireball/app/_components/summary_metrics/Display.jsx
+++ fireball/app/_components/summary_metrics/Display.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+"use client";
+
+import { useState } from "react";
+import Map from "./Map";
+import BarChart from "./BarChart";
 
 const Display = () => {
-  return <div></div>;
+  const [graphicDisplay, setGraphicDisplay] = useState("map");
+
+  return (
+    <div className="col-span-3 p-4">
+      <div>
+        <button className="border" onClick={() => setGraphicDisplay("map")}>
+          Map
+        </button>
+        <button className="border" onClick={() => setGraphicDisplay("graph")}>
+          Bar Graph
+        </button>
+      </div>
+      {graphicDisplay === "map" && <Map />}
+      {graphicDisplay === "graph" && <BarChart />}
+    </div>
+  );
 };
 
 export default Display;
